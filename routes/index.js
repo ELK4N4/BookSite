771084ea4801984bf3855b881b0e0ba615ddb637
Elkana <55286058+elkanagit@users.noreply.github.com>
2020-05-01 15:12:58 +0300

    Fixing register email section and upgrade the output

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('index.ejs');
-
-    console.log('---------------------\n New user connected! \n---------------------');
 });
 /*
 router.post('/', (req, res) => {
